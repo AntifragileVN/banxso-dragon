02467190a606e8ea5af3c8f92bef3923a7f4a054
Vitaliti Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-08-08 19:07:38 +0300

    style: auth form

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '@/services/firebase';
 import { useRecoilState } from 'recoil';
 import { currentUserState } from '@/store/user.store';
 import { FirebaseError } from 'firebase/app';
+import c from '@/components/elements/Form/Form.module.scss';
+
+import { MdOutlineAlternateEmail } from 'react-icons/md';
+import { FiLock } from 'react-icons/fi';
+import { FaGoogle } from 'react-icons/fa';
+import { Form } from 'react-router-dom';
 
 const FirebaseAuthUI: React.FC = () => {
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [emailError, setEmailError] = useState<string>('');
+	const [passwordError, setPasswordError] = useState<string>('');
+
 	const [, setCurrentUser] = useRecoilState(currentUserState);
 	const provider = new GoogleAuthProvider();
 
@@ -38,7 +49,44 @@ const FirebaseAuthUI: React.FC = () => {
 
 	return (
 		<div className="container">
-			<button onClick={onButtonClick}>SignIn with Google</button>
+			<div className={c.form__container}>
+				<form className={c.form}>
+					<h1 className={c.form__title}>Create Account</h1>
+					<div className={c.form__socials}>
+						<button className={c.form__socialsBtn} onClick={onButtonClick}>
+							<FaGoogle className={c.form__socialsIcon} />
+						</button>
+					</div>
+					<label className={c.form__label} htmlFor="logEmail">
+						Email
+					</label>
+					<div className={c.form__inputWrapper}>
+						<MdOutlineAlternateEmail className={c.form__inputIcon} />
+						<input
+							className={c.form__input}
+							type="email"
+							placeholder="example@domain.com"
+							onChange={(e) => setEmail(e.target.value)}
+							value={email}
+						/>
+					</div>
+
+					<label className={c.form__label} htmlFor="logEmail">
+						Password
+					</label>
+					<div className={c.form__inputWrapper}>
+						<FiLock className={c.form__inputIcon} />
+						<input
+							className={c.form__input}
+							type="password"
+							placeholder="●●●●●●●●●●●●"
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+						/>
+					</div>
+					<button className={c.form__submit}>Sign Up</button>
+				</form>
+			</div>
 		</div>
 	);
 };
